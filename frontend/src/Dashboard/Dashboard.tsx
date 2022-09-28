6a55ba4ec06da2181a516eb15532a3de56499f28
ngutu <gutunichita757@gmail.com>
2022-09-28 18:11:33 +0200

    dashboard test and api calls restructure

--- frontend/src/Dashboard/Dashboard.tsx
+++ frontend/src/Dashboard/Dashboard.tsx
@@ -1,5 +1,43 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config/config";
+
 export function Dashboard() {
+    const navigate = useNavigate();
+    async function handleLogout(e: React.FormEvent) {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post(`${API_URL}/logout`, {});
+            if (res.status === 200) {
+                navigate("/login");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        async function dash() {
+            try {
+                const res = await axios.post(`${API_URL}/dashboard`, {});
+                if (res.status === 200) {
+                    console.log(res);
+                }
+            } catch (err) {
+                console.log(err);
+                navigate("/login");
+            }
+        }
+
+        dash();
+    }, [])
+
     return (
-        <h2>Dashboard</h2>
+        <>
+            <h2>Dashboard</h2>
+            <form onSubmit={handleLogout}></form>
+        </>
     )
 }
---
+++ frontend/src/api/index.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+import { API_URL } from "../config/config";
+
+export async function login(user: { username: string, password: string }): Promise<boolean> {
+    try {
+        await axios.post(
+            `${API_URL}/login`, user, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+
+    return true;
+}
+
+export async function register(user: { email: string, username: string, password: string }): Promise<boolean> {
+    try {
+        await axios.post(
+            `${API_URL}/register`, user, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+
+    return true
+}
